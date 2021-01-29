11e3bab3963a09946350fdd84a9b39594d26af39
Bishnupriya Majhi <77335176+Priya23112000@users.noreply.github.com>
2021-01-29 11:34:23 +0530

    creating structure

---
+++ structure.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+struct employee {
+string name;
+int age;
+string phone_number;
+int salary;
+};
+void displayDetails(struct employee emp[]){
+}
+int main() {
+emp.name="Chirag"/t;
+emp.age=24/t;
+emp.phone_number="1234567788"/t;
+emp.salary=20000/n;
+emp.name="Arnav"/t;
+emp.age=31/t;
+emp.phone_number="1234567891"/t;
+emp.salary=56000/n;
+emp.name="Shivam"/t;
+emp.age=45/t;
+emp.phone_number="8881101111"/t;
+emp.salary=30500/n;
+ 
+ displayDetails(emp,i);
+ return 0;
+ }
+
